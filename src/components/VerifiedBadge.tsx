47752ff18dc5857fff5ca39318e11f148e6ed668
jat4 <144493972+jat4@users.noreply.github.com>
2026-07-19 15:54:30 +0530

    Update VerifiedBadge.tsx

--- src/components/VerifiedBadge.tsx
+++ src/components/VerifiedBadge.tsx
@@ -5,21 +5,22 @@ interface VerifiedBadgeProps {
   isGold?: boolean;
 }
 
-export default function VerifiedBadge({ className = "w-4 h-4", isGold = false }: VerifiedBadgeProps) {
+export default function VerifiedBadge({ className = "w-[18px] h-[18px]", isGold = false }: VerifiedBadgeProps) {
+  const fillColor = isGold ? "#FFD700" : "rgb(0, 149, 246)";
   return (
     <svg
-      viewBox="0 0 24 24"
+      aria-label="Verified"
       className={`${className} inline-block ml-1 align-middle shrink-0`}
-      aria-label="Verified Profile"
+      fill={fillColor}
+      role="img"
+      viewBox="0 0 40 40"
     >
+      <title>Verified</title>
       <path
-        fill={isGold ? "#FFD700" : "#0095f6"}
-        d="M12.003 2.001a1.2 1.2 0 0 1 .945.459l1.196 1.492a1.2 1.2 0 0 0 .935.432l1.905.022a1.2 1.2 0 0 1 1.189 1.189l.022 1.905a1.2 1.2 0 0 0 .432.935l1.492 1.196a1.2 1.2 0 0 1 .459.945a1.2 1.2 0 0 1-.459.945l-1.492 1.196a1.2 1.2 0 0 0-.432.935l-.022 1.905a1.2 1.2 0 0 1-1.189 1.189l-1.905.022a1.2 1.2 0 0 0-.935.432l-1.196 1.492a1.2 1.2 0 0 1-.945.459a1.2 1.2 0 0 1-.945-.459l-1.196-1.492a1.2 1.2 0 0 0-.935-.432l-1.905-.022a1.2 1.2 0 0 1-1.189-1.189l-.022-1.905a1.2 1.2 0 0 0-.432-.935l-1.492-1.196a1.2 1.2 0 0 1-.459-.945a1.2 1.2 0 0 1 .459-.945l1.492-1.196a1.2 1.2 0 0 0 .432-.935l.022-1.905a1.2 1.2 0 0 1 1.189-1.189l1.905-.022a1.2 1.2 0 0 0 .935-.432l1.196-1.492a1.2 1.2 0 0 1 .945-.459z"
-      />
-      <path
-        fill={isGold ? "#000000" : "#ffffff"}
-        d="M10.75 15.3l5.5-5.5a.8.8 0 0 0-1.13-1.13l-4.37 4.37l-1.87-1.87a.8.8 0 1 0-1.13 1.13l2.5 2.5a.8.8 0 0 0 1.13 0z"
+        d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z"
+        fillRule="evenodd"
       />
     </svg>
   );
 }
+
